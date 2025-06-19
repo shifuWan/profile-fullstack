cf50f292ec71a8d06152a6e8ff9445e03bd66e8f
almer06 <almer.maulana1234@gmail.com>
2025-06-19 21:27:37 +0700

    feat: add FooterComponent, NavigationBar, and SectionPage components for improved layout and navigation

---
+++ frontend/components/NavigationBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="bg-white shadow h-16">
+    <div class="flex justify-between items-center h-full px-4 container mx-auto">
+      <h1 class="font-itim text-2xl font-bold">My Website</h1>
+      <nav>
+        <ul class="flex gap-4">
+          <li>
+            <NuxtLink to="/" class="text-blue-500 font-itim text-xl hover:underline"
+              >Work</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink to="/#about" class="text-blue-500 font-itim text-xl hover:underline"
+              >About Me</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink to="/#blog" class="text-blue-500 font-itim text-xl hover:underline"
+              >Blog</NuxtLink
+            >
+          </li>
+          <li>
+            <NuxtLink to="/contact" class="text-blue-500 font-itim text-xl hover:cursor-pointer px-4 py-2 rounded-md bg-blue-100 hover:bg-blue-200"
+              >Contact</NuxtLink
+            >
+          </li>
+        </ul>
+      </nav>
+      <Icon name="mdi:hamburger-menu" class="text-2xl md:hidden" />
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
---
+++ frontend/components/SectionPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <section :id="id" :class="mergedClass">
+    <div class="container mx-auto px-4 py-8">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import { twMerge } from 'tailwind-merge'
+
+const props = defineProps<{
+  id: string
+  class?: string
+}>()
+
+const defaultClass =
+  'bg-slate-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100'
+
+const mergedClass = computed(() => twMerge(defaultClass, props.class))
+</script>
