38a7ec1a212aa7c2d13743f7b3a5c95565800b55
almer06 <almer.maulana1234@gmail.com>
2025-06-19 21:27:48 +0700

    feat: add default layout with NavigationBar and Footer components

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <UApp>
+    <div class="flex flex-col min-h-screen">
+        <NavigationBar />
+        <main class="flex-1">
+            <slot />
+        </main>
+        <Footer />
+    </div>
+  </UApp>
+</template>
+
+<style scoped></style>
